dc577a4eae1e4d29745d45018206b3b6dcce3df8
ComeDobe <christiandobe@hotmail.fr>
2024-02-27 22:23:02 +0100

    nouvel commit

--- src/main/java/com/dobe/WebSocket/config/WebSocketEventListener.java
+++ src/main/java/com/dobe/WebSocket/config/WebSocketEventListener.java
@@ -3,6 +3,7 @@ package com.dobe.WebSocket.config;
 import com.dobe.WebSocket.model.Message;
 import com.dobe.WebSocket.model.MessageType;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
@@ -12,6 +13,7 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class WebSocketEventListener {
 
     private final SimpMessagingTemplate simpMessagingTemplate;
